35ebc60bb53d85ced874cddd223042fa80b9b8e5
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-12-20 14:06:38 -0800

    pyracantha initial commit

---
+++ Pyracantha/main.py
@@ -0,0 +1,10 @@
+'''
+Little monologue about Pyracantha jam
+'''
+text = [["That's Pyracantha, fire thorn."],
+["Don't eat the seeds, they contain cyanide."], 
+["The flesh doesn't taste too good, either."], 
+["I heard they taste real good as jam."], 
+["...but of course it would taste good, jam is just fruit with an obscene amount of sugar."], 
+["Why spend all that time straining seeds from a tiny berry when you can use domesticated fruit..."]]
+
