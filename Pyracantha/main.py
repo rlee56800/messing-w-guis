3d32783e3dfbcf51ee7d3dfb7cfd0572b86eddb3
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-12-21 03:04:46 -0800

    nvm I'm dumb

--- Pyracantha/main.py
+++ Pyracantha/main.py
@@ -4,12 +4,12 @@ Little monologue about Pyracantha jam
 import tkinter
 import time
 
-words = [["That's Pyracantha, fire thorn."],
-["Don't eat the seeds, they contain cyanide."], 
-["The flesh doesn't taste too good, either."], 
-["I heard they taste real good as jam."], 
-["...but of course it would taste good, jam is just fruit with an obscene amount of sugar."], 
-["Why spend all that time straining seeds from a tiny berry when you can use domesticated fruit..."]]
+words = ["That's Pyracantha, fire thorn.",
+"Don't eat the seeds, they contain cyanide.", 
+"The flesh doesn't taste too good, either.", 
+"I heard they taste real good as jam.", 
+"...but of course it would taste good, jam is just fruit with an obscene amount of sugar.", 
+"Why spend all that time straining seeds from a tiny berry when you can use domesticated fruit..."]
 
 master = tkinter.Tk(className="Pyracantha Jam")
 master.geometry("600x400")
